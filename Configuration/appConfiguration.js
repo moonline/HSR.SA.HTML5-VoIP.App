2ce0fa326bdebb41aa3746ce06b8b2144e66ecd7
Tobias Blaser <tobiasblaser@colvarim.ch>
2013-11-05 17:06:44 +0100

    VCard correct implementation

--- Configuration/appConfiguration.js
+++ Configuration/appConfiguration.js
@@ -50,5 +50,70 @@ App.Configuration = {
 		"online": [
 
 		]
-	}
+	},
+	"contactbookVcardConfig": [
+		{
+			"path": ["VERSION"],
+			"mapTo": ['version']
+		},{
+			"path": ["N"],
+			"mapTo": {
+				0: ['lastname'],
+				1: ['firstname']
+			}
+		},{
+			"path": ["FN"],
+			"mapTo": ['name']
+		},{
+			"path": ["ADR", "HOME", "POSTAL"],
+			"mapTo": {
+				2: ['address','street'],
+				3: ['address','city'],
+				4: ['address','region'],
+				5: ['address','postcode'],
+				6: ['address','country']
+			}
+		},{
+			"path": ["NICKNAME"],
+			"mapTo": ['nickname']
+		},{
+			"path": ["TEL", "CELL", "VOICE"],
+			"mapTo": ['phone'],
+			"mappingRule": 'list'
+		},{
+			"path": ["TEL", "HOME","VOICE"],
+			"mapTo": ['phone'],
+			"mappingRule": 'list'
+		},{
+			"path": ["TEL"],
+			"mapTo": ['phone'],
+			"mappingRule": 'list'
+		},{
+			"path": ["EMAIL", "PREF", "INTERNET"],
+			"mapTo": ['email'],
+			"mappingRule": 'list'
+		},{
+			"path": ["EMAIL", "INTERNET"],
+			"mapTo": ['email'],
+			"mappingRule": 'list'
+		},{
+			"path": ["BDAY"],
+			"mapTo": ['birthdate']
+		},{
+			"path": ["X-SIP"],
+			"mapTo": ["sip"]
+		},{
+			"path": ["X-SIP"],
+			"mapTo": ["sip"]
+		},{
+			"path": ["PHOTO"],
+			"mapTo": ["photo"]
+		},{
+			"path": ["ORG"],
+			"mapTo": ["organisation"]
+		},{
+			"path": ["URL"],
+			"mapTo": ["website"]
+		}
+	]
 };
--- Src/Model/Domain/Addressbook/AddressbookVcard.js
+++ Src/Model/Domain/Addressbook/AddressbookVcard.js
@@ -3,6 +3,7 @@
 (function () {
 	var Domain = App.Model.Domain;
 	var Addressbook = Domain.Addressbook;
+	var Configuration = App.Configuration;
 
 
 	Addressbook.AddressbookVcard = function () {
@@ -11,11 +12,72 @@
 		this.data = new Array();
 		this.dataSourceTypes = Domain.Addressbook.dataSourceTypes.file;
 
-		this.fieldMapping = {
-			"N": ['lastName', 'firstName'],
-			"X-SIP": 'sip',
-			"PHOTO": 'photo'
-		};
+		// TODO: move to config
+		this.fieldMapping = (Configuration.contactbookVcardConfig) ? Configuration.contactbookVcardConfig : [
+			{
+				"path": ["VERSION"],
+				"mapTo": ['version']
+			},{
+				"path": ["N"],
+				"mapTo": {
+					0: ['lastname'],
+					1: ['firstname']
+				}
+			},{
+				"path": ["FN"],
+				"mapTo": ['name']
+			},{
+				"path": ["ADR", "HOME", "POSTAL"],
+				"mapTo": {
+					2: ['address','street'],
+					3: ['address','city'],
+					4: ['address','region'],
+					5: ['address','postcode'],
+					6: ['address','country']
+				}
+			},{
+				"path": ["NICKNAME"],
+				"mapTo": ['nickname']
+			},{
+				"path": ["TEL", "CELL", "VOICE"],
+				"mapTo": ['phone'],
+				"mappingRule": 'list'
+			},{
+				"path": ["TEL", "HOME","VOICE"],
+				"mapTo": ['phone'],
+				"mappingRule": 'list'
+			},{
+				"path": ["TEL"],
+				"mapTo": ['phone'],
+				"mappingRule": 'list'
+			},{
+				"path": ["EMAIL", "PREF", "INTERNET"],
+				"mapTo": ['email'],
+				"mappingRule": 'list'
+			},{
+				"path": ["EMAIL", "INTERNET"],
+				"mapTo": ['email'],
+				"mappingRule": 'list'
+			},{
+				"path": ["BDAY"],
+				"mapTo": ['birthdate']
+			},{
+				"path": ["X-SIP"],
+				"mapTo": ["sip"]
+			},{
+				"path": ["X-SIP"],
+				"mapTo": ["sip"]
+			},{
+				"path": ["PHOTO"],
+				"mapTo": ["photo"]
+			},{
+				"path": ["ORG"],
+				"mapTo": ["organisation"]
+			},{
+				"path": ["URL"],
+				"mapTo": ["website"]
+			}
+		];
 	};
 
 
@@ -36,38 +98,81 @@
 		var entry = new Domain.AddressbookEntry();
 
 		var lines = vcardContent.split('\n');
-		console.log(vcardContent);
 		lines.forEach(function (line) {
 			if (line != '' && line != "BEGIN:VCARD" && line != "END:VCARD") {
 				var fieldParts = line.splitOnce(':');
 				var fieldHeader = fieldParts[0];
 				var headerParts = fieldHeader.split(';');
-				var fieldName = headerParts[0];
-
-				// Todo: implement structuring from header attributes
 				var fieldContent = fieldParts[1];
 				var values = fieldContent.split(';');
 				values.trimElements();
 
-				//entry[fieldName] = { properties: {}, value: values };
-				if (mapping.hasOwnProperty(fieldName)) {
-					if (mapping[fieldName] instanceof Array) {
-						mapping[fieldName].forEach(function (targetField, index) {
-							if (values[index]) {
-								entry[targetField] = values[index];
-							}
-						}, this);
+				var data = {
+					"path": [],
+					"properties": [],
+					"values": values
+				};
+				headerParts.forEach(function(property){
+					if(property.indexOf('=') < 0) {
+						data.path.push(property);
 					} else {
-						var newKey = mapping[fieldName];
-						entry[newKey] = values[0];
+						data.properties.push({ "property": property.splitOnce('=')[0], "value": property.splitOnce('=')[1] })
 					}
-				}
+				});
+
+				this.fieldMapping.forEach(function(field){
+					if(JSON.stringify(field.path) === JSON.stringify(data.path)) {
+						if(field.mapTo instanceof Array) {
+							var parentField = entry;
+							var currentKey;
+
+							field.mapTo.forEach(function(segment){
+								if(!currentKey) {
+									currentKey = segment;
+								} else {
+									if(!parentField[currentKey]) {
+										parentField[currentKey] = new Object();
+									}
+									parentField = parentField[currentKey];
+									currentKey = segment;
+								}
+							},this);
+
+							if(field.mappingRule === 'list') {
+								if(parentField[currentKey] instanceof Array) {
+									parentField[currentKey] = parentField[currentKey].concat(values);
+								} else {
+									parentField[currentKey] = values;
+								}
+							} else {
+								parentField[currentKey] = values[0];
+							}
+						} else if(typeof(field.mapTo) === 'object') {
+							Object.keys(field.mapTo).forEach(function(key){
+								var parentField = entry;
+								var currentKey;
+
+								field.mapTo[key].forEach(function(segment){
+									if(!currentKey) {
+										currentKey = segment;
+									} else {
+										if(!parentField[currentKey]) {
+											parentField[currentKey] = new Object();
+										}
+										parentField = parentField[currentKey];
+										currentKey = segment;
+									}
+								},this);
+								parentField[currentKey] = values[key];
+							},this);
+						}
+					}
+				},this);
 			}
 		}, this);
 		if (!entry.name && entry.firstName && entry.lastName) {
 			entry.name = entry.firstName + ' ' + entry.lastName;
 		}
-
 		if (Object.keys(entry).length > 0) {
 			this.data.push(entry);
 		}
