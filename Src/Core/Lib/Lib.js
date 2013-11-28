61d0680afd0e0d4f14e427dffb959d05b126196b
Jannis Grimm <jannis@gje.ch>
2013-11-28 16:47:40 +0100

    First test

--- Src/Core/Lib/Lib.js
+++ Src/Core/Lib/Lib.js
@@ -3,8 +3,7 @@
 
 	var using = App.Core.Framework.namespace;
 	var Lib = using('App.Core.Lib');
-	Lib.Handlebars = Handlebars;
-	Lib.jQuery = jQuery.noConflict();
+	//Lib.jQuery = jQuery.noConflict();
 	//Lib.jQuery = jQuery.noConflict(true); // delete global jquery variable
 
 })();
--- Src/initialize.js
+++ Src/initialize.js
@@ -4,11 +4,13 @@
 	var using = App.Core.Framework.namespace;
 	var Controller = using('App.Controller');
 
-
-	window.onload = function() {
-		var accountController = new Controller.AccountController();
-		accountController.addDummyUser();
-		//accountController.selectUser();
-	};
+	var accountController = new Controller.AccountController();
+	accountController.addDummyUser();
+	//accountController.selectUser();
+	
+	var app = angular.module('app', []);
+	app.controller('AM', function ($scope) {
+		$scope.accountManager = accountController.accountManager;
+	});
 
 })();
